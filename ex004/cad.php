f14af68f69dd5e9c76d2c5a3bef50b76298de890
Eduardo Alvarenga <eduardoalvarenga.jp@hotmail.com>
2024-09-18 23:46:49 -0300

    ajuste final

---
+++ ex004/cad.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-bt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado do Processamento</title>
+</head>
+<body>
+    <header>
+        <h1>Resultado do Processamento</h1>
+    </header>
+    <main>
+        <?php 
+        $nome = $_GET["nome"];
+        $sobrenome = $_GET["sobrenome"];
+        echo "Olá, seja bem vindo ao meu site $nome $sobrenome!";
+        ?>
+    </main>
+</body>
+</html>
